9e9918a6c28b1d1e17130fdf828eb1bb8c877390
Mike D <mcd8604@users.noreply.github.com>
2024-01-18 01:57:12 -0500

    dragTab update

--- EngraverMasqueAdapter.lua
+++ EngraverMasqueAdapter.lua
@@ -2,6 +2,20 @@ local addonName, Addon = ...
 
 local MasqueAdapter = {}
 
+local function OnRuneButtonsGroupCallback(Group, Option, Value)
+	if Option == "Disabled" then
+		if Value then
+			EngraverFrame:LoadCategories()
+		end
+	end
+end
+
+local function OnDragTabGroupCallback(Group, Option, Value)
+	if Option == "Disabled" and EngraverFrame.dragTab.NineSlice:CanChangeProtectedState() then
+		EngraverFrame.dragTab.NineSlice:SetShown(Value)
+	end
+end
+
 local originalLoadCategories = EngraverFrameMixin.LoadCategories
 EngraverFrameMixin.LoadCategories = function(...)
 	originalLoadCategories(...)
@@ -15,14 +29,25 @@ EngraverFrameMixin.LoadCategories = function(...)
 	end
 end
 
+local originalUpdateDragTabLayout = EngraverFrameMixin.UpdateDragTabLayout
+EngraverFrameMixin.UpdateDragTabLayout = function(...)
+	originalUpdateDragTabLayout(...)
+	if Addon.MasqueGroups and Addon.MasqueGroups.DragTab then
+		Addon.MasqueGroups.DragTab:ReSkin(EngraverFrame.dragTab)
+	end
+end
+
 EventRegistry:RegisterFrameEventAndCallback("PLAYER_LOGIN", function(event, addonName)
 	if LibStub then
 		local Masque = LibStub("Masque", true)
 		if Masque then
 			Addon.MasqueGroups = {}
 			Addon.MasqueGroups.RuneButtons = Masque:Group("Engraver", "Rune Buttons")
-			--Addon.MasqueGroups.DragTab = Masque:Group("Engraver", "Drag Tab")
-			--Addon.MasqueGroups.DragTab:AddButton(EngraverFrame.dragTab)
+			Addon.MasqueGroups.RuneButtons:RegisterCallback(OnRuneButtonsGroupCallback, "Disabled")
+			Addon.MasqueGroups.DragTab = Masque:Group("Engraver", "Drag Tab")
+			Addon.MasqueGroups.DragTab:RegisterCallback(OnDragTabGroupCallback, "Disabled")
+			Addon.MasqueGroups.DragTab:AddButton(EngraverFrame.dragTab)
+			EngraverFrame.dragTab.NineSlice:SetShown(Addon.MasqueGroups.DragTab.db.Disabled)
 			--Addon.MasqueGroups.FilterSelector = Masque:Group("Engraver", "Filter Selector")
 			--Addon.MasqueGroups.FilterSelector:AddButton(EngraverFrame.filterRightButton)
 			--Addon.MasqueGroups.FilterSelector:AddButton(EngraverFrame.filterLeftButton)
