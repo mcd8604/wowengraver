964f848fc26ae91fc0fba21249506cb77bdc340f
Mike D <mcd8604@users.noreply.github.com>
2024-01-20 22:49:57 -0500

    start replace filters dropdown with list
    working poc, need to improve visually and add right click context menu

--- EngraverOptions.lua
+++ EngraverOptions.lua
@@ -23,7 +23,6 @@ function EngraverOptionsFrameMixin:OnLoad()
 	self:InitSettingsList()
 	self:CreateSettingsInitializers()
 	self.settingsList:Display(self.initializers);
-	EngraverOptionsCallbackRegistry:RegisterCallback("CurrentFilter", self.OnCurrentFilterChanged, self)
 end
 
 function EngraverOptionsFrameMixin:InitSettingsList()
@@ -64,7 +63,9 @@ end
 local function AddInitializer(self, initializer)
 	if initializer then	
 		table.insert(self.initializers, initializer);
-		initializer:AddSearchTags(initializer:GetName():gmatch("%S+"))
+		if initializer.GetName then
+			initializer:AddSearchTags(initializer:GetName():gmatch("%S+"))
+		end
 	end
 end
 
@@ -111,30 +112,13 @@ function EngraverOptionsFrameMixin:CreateSettingsInitializers()
 		options:SetLabelFormatter(MinimalSliderWithSteppersMixin.Label.Right, FormatPercentage);
 		AddInitializer(self, Settings.CreateSliderInitializer(setting, options, tooltip))
 	end	-- UIScale
-	AddInitializer(self, CreateSettingsListSectionHeaderInitializer("Filters"));
 	do -- ShowFilterSelector
 		AddInitializer(self, Settings.CreateCheckBoxInitializer(AddEngraverOptionsSetting(self, "ShowFilterSelector", "Show Filter Selector", Settings.VarType.Boolean), nil, "Shows the filter selector."))
 	end -- ShowFilterSelector
-	do -- FilterDropDown
-		local function GetOptions()
-			local container = Settings.CreateControlTextContainer();
-			container:Add(0, Addon.Filters.NO_FILTER_DISPLAY_STRING);
-			for i, filter in ipairs(Addon.Filters:GetFiltersForPlayerClass()) do
-				container:Add(i, filter.Name);
-			end
-			return container:GetData();
-		end
-		local variable, name, tooltip = "CurrentFilter", "Current Filter", "Changes the currently active filter.";
-		local setting = AddEngraverOptionsSetting(self, variable, name, Settings.VarType.Number)
-		AddInitializer(self, Settings.CreateControlInitializer("EngraverOptionsFilterDropDownTemplate", setting, GetOptions, tooltip))
-	end -- FilterDropDown
-	do -- FilterControl
-		local variable, name, tooltip = "FilterData", "Selected Runes", "Selected runes are shown. Deselected runes are hidden.";
-		local setting = Settings.RegisterAddOnSetting(self.category, name, variable, Settings.VarType.Boolean, false);
-		local initializer = Settings.CreateControlInitializer("EngraverOptionsFilterControlTemplate", setting, nil, tooltip)
-		initializer:AddShownPredicate(function() return Addon.Filters:IsCurrentFilterValid(); end);
-		AddInitializer(self, initializer)
-	end -- FilterControl
+	AddInitializer(self, CreateSettingsListSectionHeaderInitializer("Filters"));
+	do -- FilterEditor
+		table.insert(self.initializers, Settings.CreateElementInitializer("EngraverOptionsFilterEditorTemplate", { settings = {} } ));
+	end -- FilterEditor
 end
 
 function EngraverOptionsFrameMixin:OnEvent(event, ...)
@@ -174,131 +158,4 @@ function EngraverOptionsFrameMixin:OnRefresh()
 			end
 		end
 	end
-end
-
---function EngraverOptionsFrameMixin:OnCommit()
---	print("OnCommit")
---end
-
-function EngraverOptionsFrameMixin:OnCurrentFilterChanged(_, newValue)
-	if not InCombatLockdown() then
-		-- RepairDisplay will help remove the FilterControl when there is no current filter and add it when there is
-		self.settingsList:RepairDisplay(self.initializers)	
-	end
-end
-
---------------------
--- FilterDropDown --
---------------------
-
-EngraverOptionsFilterDropDownMixin = CreateFromMixins(SettingsDropDownControlMixin)
-
-function EngraverOptionsFilterDropDownMixin:OnLoad()
-	SettingsDropDownControlMixin.OnLoad(self);
-
-	self.NewButton:ClearAllPoints();
-	self.NewButton:SetPoint("TOPRIGHT", self.DropDown.Button, "BOTTOM");
-
-	self.DeleteButton:ClearAllPoints();
-	self.DeleteButton:SetPoint("TOPLEFT", self.NewButton, "TOPRIGHT", -10);
-end
-
-function EngraverOptionsFilterDropDownMixin:Init(initializer)
-	SettingsDropDownControlMixin.Init(self, initializer);
-
-	self.NewButton:SetText("New Filter"); -- TODO localization
-	self.NewButton:SetScript("OnClick", function()
-		StaticPopup_Show("ENGRAVER_FILTER_NEW", nil, nil, { filterDropDown = self });
-	end);
-
-	self.DeleteButton:SetText(DELETE);
-	self.DeleteButton:SetScript("OnClick", function()
-		if EngraverOptions.CurrentFilter then
-			StaticPopup_Show("ENGRAVER_FILTER_DELETION", Addon.Filters:GetCurrentFilterName(), nil, { filterDropDown = self } );
-		end
-	end);
-
-	-- TODO rename filter
-	-- TODO re-order up/down
-
-	self:RefreshSelected();
-	self:EvaluateButtonState();
-	-- Update selected value when EngraverOption.CurrentFilter changes
-	EngraverOptionsCallbackRegistry:RegisterCallback("CurrentFilter", function(_, newIndex)
-		if not InCombatLockdown() then 
-			self:SetValue(newIndex)
-			self:EvaluateButtonState()
-		end
-	end, self)
-end
-
-do
-	local function OnCreateNewFilter(dialog)
-		local newFilterName = strtrim(dialog.editBox:GetText());
-		local index = Addon.Filters:CreateFilter(newFilterName)
-		dialog.data.filterDropDown:RefreshFilterList(index);
-		dialog:Hide();
-	end
-
-	StaticPopupDialogs["ENGRAVER_FILTER_NEW"] = {
-		text = "Create a New Filter",
-		button1 = CREATE,
-		button2 = CANCEL,
-		OnAccept = function(self)
-			PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
-			OnCreateNewFilter(self)
-		end,
-		EditBoxOnTextChanged = function(self)
-			if ( strtrim(self:GetText()) == "" ) then
-				self:GetParent().button1:Disable();
-			else
-				self:GetParent().button1:Enable();
-			end
-		end,
-		EditBoxOnEnterPressed = function(self)
-			OnCreateNewFilter(self:GetParent())
-		end,
-		exclusive = 1,
-		whileDead = 1,
-		hideOnEscape = 1,
-		hasEditBox = 1,
-		maxLetters = 31
-	};
-end
-
-StaticPopupDialogs["ENGRAVER_FILTER_DELETION"] = {
-	text = CONFIRM_COMPACT_UNIT_FRAME_PROFILE_DELETION,
-	button1 = DELETE,
-	button2 = CANCEL,
-	OnAccept = function(self)
-		PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
-		Addon.Filters:DeleteCurrentFilter()
-		-- NOTE if deleting is desired from multiple places in the future, then:
-		--      define/trigger a FilterDeleted event somewhere, register in the dropdown, and call RefreshFilterList from the handler instead
-		self.data.filterDropDown:RefreshFilterList(EngraverOptions.CurrentFilter)
-	end,
-	exclusive = 1,
-	whileDead = 1,
-	showAlert = 1,
-	hideOnEscape = 1
-};
-
-function EngraverOptionsFilterDropDownMixin:RefreshFilterList(selectedIndex)
-	self:InitDropDown();
-	self:SetValue(selectedIndex);
-	self:EvaluateButtonState();
-end
-
-function EngraverOptionsFilterDropDownMixin:RefreshSelected()
-	self:SetValue(EngraverOptions.CurrentFilter or 0)
-end
-
--- OnSettingValueChanged fires when the setting bound to control changes value (not EngraverOptions.CurrentFilter)
-function EngraverOptionsFilterDropDownMixin:OnSettingValueChanged(setting, value)
-	SettingsDropDownControlMixin.OnSettingValueChanged(self, setting, value);
-	self:EvaluateButtonState()
-end
-
-function EngraverOptionsFilterDropDownMixin:EvaluateButtonState()
-	self.DeleteButton:SetEnabled(Addon.Filters:IsCurrentFilterValid());
 end
--- EngraverOptionsFilterControl.lua
+++ EngraverOptionsFilterControl.lua
@@ -1,5 +1,14 @@
 local _, Addon = ...
 
+local selectionBehavior; -- Initialized in EngraverOptionsFilterListMixin:OnLoad
+
+local function GetSelectedFilterIndex()
+	local selectedElementData = selectionBehavior:GetFirstSelectedElementData()
+	if selectedElementData and selectedElementData.data then
+		return selectedElementData.data.filterIndex
+	end
+end
+
 -----------------------------
 -- OptionsFilterRuneButton --
 -----------------------------
@@ -8,7 +17,7 @@ EngraverOptionsFilterRuneButtonMixin = CreateFromMixins(EngraverRuneButtonMixin)
 
 function EngraverOptionsFilterRuneButtonMixin:OnClick(button, down)
 	if self.skillLineAbilityID then
-		Addon.Filters:ToggleRune(EngraverOptions.CurrentFilter, self.skillLineAbilityID, self:GetChecked())
+		Addon.Filters:ToggleRune(GetSelectedFilterIndex(), self.skillLineAbilityID, self:GetChecked())
 	end
 end
 
@@ -23,16 +32,13 @@ function EngraverOptionsFilterCategoryFrameMixin:OnLoad()
 	self.runeButtons = {}
 end
 
---------------------------
--- OptionsFilterControl --
---------------------------
+--------------------
+-- EquipmentSlots --
+--------------------
 
-EngraverOptionsFilterControlMixin = CreateFromMixins(SettingsListElementMixin);
+EngraverOptionsFilterEquipmentSlotsMixin = {}
 
-local playerClassName, _, _ = UnitClass("player")
-local ClassesRuneData = {}
-
-function EngraverOptionsFilterControlMixin:OnLoad()
+function EngraverOptionsFilterEquipmentSlotsMixin:OnLoad()
 	SettingsListElementMixin.OnLoad(self)
 	self.categoryFrames = {
 		[INVSLOT_CHEST]	=	self.chestFrame,
@@ -40,25 +46,16 @@ function EngraverOptionsFilterControlMixin:OnLoad()
 		[INVSLOT_HAND]	=	self.handsFrame
 	}
 	self:SetupCategoryFrames()
-	self:RegisterEvent("RUNE_UPDATED");
-	self:RegisterEvent("PLAYER_EQUIPMENT_CHANGED");
-	self:RegisterEvent("NEW_RECIPE_LEARNED");
-	self:RegisterEvent("UPDATE_INVENTORY_ALERTS");
-	-- Update selected runes when EngraverOptions.CurrentFilter changes
-	EngraverOptionsCallbackRegistry:RegisterCallback("CurrentFilter", function() 
-		if not InCombatLockdown() then 
-			self:SetupCategoryFrames()
-		end
-	end, self)
 end
 
-function EngraverOptionsFilterControlMixin:SetupCategoryFrames()
+function EngraverOptionsFilterEquipmentSlotsMixin:SetupCategoryFrames()
 	for category, frame in pairs(self.categoryFrames) do
 		self:SetupCategoryFrame(category, frame)
 	end
+	self:SetFilter(Addon.Filters:GetFilter(GetSelectedFilterIndex()))
 end
 
-function EngraverOptionsFilterControlMixin:SetupCategoryFrame(category, frame)
+function EngraverOptionsFilterEquipmentSlotsMixin:SetupCategoryFrame(category, frame)
 	local runes = C_Engraving.GetRunesForCategory(category, false);
 	if runes then
 		frame.category = category
@@ -66,15 +63,219 @@ function EngraverOptionsFilterControlMixin:SetupCategoryFrame(category, frame)
 		frame:SetDisplayMode(EngraverCategoryFrameShowAllMixin)
 		frame:UpdateCategoryLayout(Addon.EngraverLayoutDirections[1])
 		for r, runeButton in ipairs(frame.runeButtons) do
-			local passes = Addon.Filters:RunePassesFilter(runeButton)
-			runeButton:SetChecked(passes)
 			runeButton:SetBlinking(false)
+			runeButton:SetEnabled(false)
+			runeButton.Border:SetShown(false)
+			runeButton.icon:SetDesaturated(true)
 		end
 	end
 end
 
-function EngraverOptionsFilterControlMixin:OnEvent(event, ...)
+function EngraverOptionsFilterEquipmentSlotsMixin:SetFilter(filter)
 	for category, frame in pairs(self.categoryFrames) do
-		frame:UpdateCategoryLayout(Addon.EngraverLayoutDirections[1])
+		for r, runeButton in ipairs(frame.runeButtons) do
+			if filter then
+				local passes = Addon.Filters:RunePassesFilter(runeButton, filter)
+				runeButton:SetChecked(passes)
+				runeButton:SetEnabled(true)
+				runeButton.icon:SetDesaturated(false)
+			else
+				runeButton:SetChecked(false)
+				runeButton:SetEnabled(false)
+				runeButton.icon:SetDesaturated(true)
+			end
+		end
 	end
+end
+
+------------------
+-- FilterEditor --
+------------------
+
+EngraverOptionsFilterEditorMixin = CreateFromMixins(SettingsListElementMixin);
+
+function EngraverOptionsFilterEditorMixin:OnLoad()
+	SettingsListElementMixin.OnLoad(self)
+	selectionBehavior:RegisterCallback(SelectionBehaviorMixin.Event.OnSelectionChanged, self.OnSelectedFilterChanged, self);
+	self.Tooltip:SetShown(false)
+	self.NewButton:SetScript("OnClick", function()
+		StaticPopup_Show("ENGRAVER_FILTER_NEW", nil, nil, { filterList = self.filterList });
+	end);
+	self.DeleteButton:SetScript("OnClick", function()
+		local elementData = selectionBehavior:GetFirstSelectedElementData()
+		if elementData and elementData.data and elementData.data.filter and elementData.data.filter.Name then
+			StaticPopup_Show("ENGRAVER_FILTER_DELETION", elementData.data.filter.Name, nil, { filterList = self.filterList });
+		end
+	end);
+end
+
+function EngraverOptionsFilterEditorMixin:OnSelectedFilterChanged(elementData, selected)
+	if not InCombatLockdown() then 
+		if selected then
+			self.equipmentSlotsFrame:SetFilter(elementData.data.filter)
+		end
+	end
+end
+
+do
+	local function OnCreateNewFilter(dialog)
+		local newFilterName = strtrim(dialog.editBox:GetText());
+		local index = Addon.Filters:CreateFilter(newFilterName)
+		dialog.data.filterList:LoadFilterData();
+		local elements = dialog.data.filterList.elementList
+		selectionBehavior:SelectElementData(elements[index])
+		dialog:Hide();
+	end
+
+	StaticPopupDialogs["ENGRAVER_FILTER_NEW"] = {
+		text = "Create a New Filter",
+		button1 = CREATE,
+		button2 = CANCEL,
+		OnAccept = function(self)
+			PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
+			OnCreateNewFilter(self)
+		end,
+		EditBoxOnTextChanged = function(self)
+			if ( strtrim(self:GetText()) == "" ) then
+				self:GetParent().button1:Disable();
+			else
+				self:GetParent().button1:Enable();
+			end
+		end,
+		EditBoxOnEnterPressed = function(self)
+			OnCreateNewFilter(self:GetParent())
+		end,
+		exclusive = 1,
+		whileDead = 1,
+		hideOnEscape = 1,
+		hasEditBox = 1,
+		maxLetters = 31
+	};
+end
+
+StaticPopupDialogs["ENGRAVER_FILTER_DELETION"] = {
+	text = CONFIRM_COMPACT_UNIT_FRAME_PROFILE_DELETION,
+	button1 = DELETE,
+	button2 = CANCEL,
+	OnAccept = function(self)
+		PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
+		local filterIndex = GetSelectedFilterIndex()
+		if filterIndex > 0 then
+			Addon.Filters:DeleteFilter(filterIndex)
+			-- NOTE if deleting is desired from multiple places in the future, then:
+			--      define/trigger a FilterDeleted event in Filters, register in the editor, and move the code below to the handler
+			self.data.filterList:LoadFilterData();
+			local dataProvider = self.data.filterList.dataProvider
+			if dataProvider then
+				local elementData = dataProvider:Find(filterIndex) or dataProvider:Find(filterIndex-1);
+				if elementData then
+					selectionBehavior:SelectElementData(elementData)
+				end
+			end
+		end
+	end,
+	exclusive = 1,
+	whileDead = 1,
+	showAlert = 1,
+	hideOnEscape = 1
+};
+
+----------------
+-- FilterList --
+----------------
+
+EngraverOptionsFilterListMixin = {} 
+
+function EngraverOptionsFilterListMixin:OnLoad()
+	local function Factory(factory, elementData)
+		local function Initializer(button, elementData)
+			elementData:InitFrame(button);
+		end
+		elementData:Factory(factory, Initializer);
+	end
+
+	local pad = 0;
+	local spacing = 2;
+	local view = CreateScrollBoxListLinearView(pad, pad, pad, pad, spacing);
+	view:SetElementFactory(Factory);
+	ScrollUtil.InitScrollBoxListWithScrollBar(self.ScrollBox, self.ScrollBar, view);
+
+	local scrollBoxAnchorsWithBar = 
+	{
+		CreateAnchor("TOPLEFT", 0, 0),
+		CreateAnchor("BOTTOMRIGHT", -16, 0);
+	};
+	local scrollBoxAnchorsWithoutBar = 
+	{
+		scrollBoxAnchorsWithBar[1],
+		CreateAnchor("BOTTOMRIGHT", 0, 0);
+	};
+	ScrollUtil.AddManagedScrollBarVisibilityBehavior(self.ScrollBox, self.ScrollBar, scrollBoxAnchorsWithBar, scrollBoxAnchorsWithoutBar);
+	
+	local function OnSelectionChanged(o, elementData, selected)
+		local button = self.ScrollBox:FindFrame(elementData);
+		if button then
+			button:UpdateStateInternal(selected);
+		end
+		if selected then
+			self.ScrollBox:ScrollToElementData(elementData, ScrollBoxConstants.AlignNearest);
+		end
+	end;
+	selectionBehavior = ScrollUtil.AddSelectionBehavior(self.ScrollBox);
+	selectionBehavior:RegisterCallback(SelectionBehaviorMixin.Event.OnSelectionChanged, OnSelectionChanged)
+	
+	-- TODO ScrollBoxDragBehavior:SetReorderable(reorderable)
+	
+	self:LoadFilterData()
+end
+
+function EngraverOptionsFilterListMixin:LoadFilterData()
+	self.elementList = {};
+	for i, filter in ipairs(Addon.Filters:GetFiltersForPlayerClass()) do
+		local initializer = CreateFromMixins(ScrollBoxFactoryInitializerMixin);
+		initializer:Init("EngraverOptionsFilterListButtonTemplate");
+		initializer.data = { filterIndex = i, filter = filter };
+		table.insert(self.elementList, initializer);
+	end
+	self.dataProvider = CreateDataProvider(self.elementList);
+	self.ScrollBox:SetDataProvider(self.dataProvider, ScrollBoxConstants.RetainScrollPosition);
+	selectionBehavior:SelectFirstElementData(function(data) return true; end);
+end
+
+----------------------
+-- FilterListButton --
+----------------------
+
+EngraverOptionsFilterListButtonMixin = CreateFromMixins(ButtonStateBehaviorMixin);
+
+function EngraverOptionsFilterListButtonMixin:UpdateStateInternal(selected)
+	if selected then
+		self.Label:SetFontObject("GameFontHighlight");
+		self.Texture:SetAtlas("Options_List_Active", TextureKitConstants.UseAtlasSize);
+		self.Texture:Show();
+	else
+		local initializer = self:GetElementData();
+		self.Label:SetFontObject("GameFontNormal");
+		if self.over then
+			self.Texture:SetAtlas("Options_List_Hover", TextureKitConstants.UseAtlasSize);
+			self.Texture:Show();
+		else
+			self.Texture:Hide();
+		end
+	end
+end
+
+function EngraverOptionsFilterListButtonMixin:OnButtonStateChanged()
+	self:UpdateStateInternal(selectionBehavior:IsSelected(self));
+end
+
+function EngraverOptionsFilterListButtonMixin:Init(initializer)
+	local filter = initializer.data.filter;
+	self.Label:SetText(filter.Name);
+	self:UpdateStateInternal(selectionBehavior:IsSelected(self));
+end
+
+function EngraverOptionsFilterListButtonMixin:OnClick(buttonName, down)
+	selectionBehavior:Select(self);
+	PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
 end
